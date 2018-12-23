fd974fabf013f3109ed251b9fe3fa2f9fad0b428
MrMcEpic <45011453+MrMcEpic@users.noreply.github.com>
2018-12-22 23:01:34 -0500

    fixed mobile zooming?

--- js/listeners.js
+++ js/listeners.js
@@ -3,8 +3,15 @@ window.addEventListener("keydown", onKeyDown, false);
 window.addEventListener("keyup", onKeyUp, false);
 myCanvas.addEventListener("mousedown", onMouseDown, false);
 myCanvas.addEventListener("mouseup", onMouseUp, false);
+myCanvas.addEventListener("touchstart", touchHandler, false);
 document.addEventListener("DOMContentLoaded", domloaded, false);
 //----\\
+
+function touchHandler(event) {
+	if (event.touches.length > 1) {
+		event.preventDefault();
+	}
+}
 
 function onKeyDown(event) {
 	if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) { //disables arrow key scrolling
